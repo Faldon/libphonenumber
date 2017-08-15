000ca33a29f23fa01b39c002a0d0a36c083476c6
Thomas Pulzer <t.pulzer@kniel.de>
2017-08-15 11:59:42 +0200

    Added public class PhoneNumber. Changed phoneNumberParser to expect phone nuber and country code to be passed as function parameters. Let phoneNumberParser return a PhoneNumber object.

--- javascript/i18n/phonenumbers/demo.js
+++ javascript/i18n/phonenumbers/demo.js
@@ -21,141 +21,85 @@
  * @author Nikolaos Trogkanis
  */
 
-goog.require('goog.dom');
-goog.require('goog.json');
-goog.require('goog.proto2.ObjectSerializer');
-goog.require('goog.string.StringBuffer');
-goog.require('i18n.phonenumbers.AsYouTypeFormatter');
 goog.require('i18n.phonenumbers.PhoneNumberFormat');
-goog.require('i18n.phonenumbers.PhoneNumberType');
 goog.require('i18n.phonenumbers.PhoneNumberUtil');
-goog.require('i18n.phonenumbers.PhoneNumberUtil.ValidationResult');
 
+/**
+ * PhoneNumber
+ * @constructor
+ * @export
+ */
+function PhoneNumber(p, r, v, nr, nt, cc, ac, sc, ds) {
+  this['IsPossible'] = p;
+  this['Reason'] = r;
+  this['IsValid'] = v;
+  this['NumberRegion'] = nr;
+  this['NumberType'] = nt;
+  this['CountryCode'] = cc;
+  this['AreaCode'] = ac;
+  this['StationCode'] = sc;
+  this['DialString'] = ds;
+  return this;
+}
 
-function phoneNumberParser() {
-  var $ = goog.dom.getElement;
-  var phoneNumber = $('phoneNumber').value;
-  var regionCode = $('defaultCountry').value;
-  var carrierCode = $('carrierCode').value;
-  var output = new goog.string.StringBuffer();
+/** @type {boolean} */
+PhoneNumber.prototype.IsPossible;
+/** @type {number} */
+PhoneNumber.prototype.Reason;
+/** @type {boolean} */
+PhoneNumber.prototype.IsValid;
+/** @type {null|string} */
+PhoneNumber.prototype.NumberRegion;
+/** @type {number} */
+PhoneNumber.prototype.NumberType;
+/** @type {number} */
+PhoneNumber.prototype.CountryCode;
+/** @type {string} */
+PhoneNumber.prototype.AreaCode;
+/** @type {string} */
+PhoneNumber.prototype.StationCode;
+/** @type {string} */
+PhoneNumber.prototype.DialString;
+
+function phoneNumberParser(sNumber, sCountry) {
+  var phoneNumber = sNumber;
+  var regionCode = sCountry;
   try {
     var phoneUtil = i18n.phonenumbers.PhoneNumberUtil.getInstance();
     var number = phoneUtil.parseAndKeepRawInput(phoneNumber, regionCode);
-    output.append('****Parsing Result:****\n');
-    output.append(goog.json.serialize(new goog.proto2.ObjectSerializer(
-        goog.proto2.ObjectSerializer.KeyOption.NAME).serialize(number)));
-    output.append('\n\n****Validation Results:****');
-    var isPossible = phoneUtil.isPossibleNumber(number);
-    output.append('\nResult from isPossibleNumber(): ');
-    output.append(isPossible);
-    if (!isPossible) {
-      output.append('\nResult from isPossibleNumberWithReason(): ');
-      var PNV = i18n.phonenumbers.PhoneNumberUtil.ValidationResult;
-      switch (phoneUtil.isPossibleNumberWithReason(number)) {
-        case PNV.INVALID_COUNTRY_CODE:
-          output.append('INVALID_COUNTRY_CODE');
-          break;
-        case PNV.TOO_SHORT:
-          output.append('TOO_SHORT');
-          break;
-        case PNV.TOO_LONG:
-          output.append('TOO_LONG');
-          break;
-      }
-      // IS_POSSIBLE shouldn't happen, since we only call this if _not_
-      // possible.
-      output.append('\nNote: numbers that are not possible have type ' +
-          'UNKNOWN, an unknown region, and are considered invalid.');
+    var p = phoneUtil.isPossibleNumber(number);
+    if (!p) {
+      var r = phoneUtil.isPossibleNumberWithReason(number);
     } else {
-      var isNumberValid = phoneUtil.isValidNumber(number);
-      output.append('\nResult from isValidNumber(): ');
-      output.append(isNumberValid);
-      if (isNumberValid && regionCode && regionCode != 'ZZ') {
-        output.append('\nResult from isValidNumberForRegion(): ');
-        output.append(phoneUtil.isValidNumberForRegion(number, regionCode));
-      }
-      output.append('\nPhone Number region: ');
-      output.append(phoneUtil.getRegionCodeForNumber(number));
-      output.append('\nResult from getNumberType(): ');
-      var PNT = i18n.phonenumbers.PhoneNumberType;
-      switch (phoneUtil.getNumberType(number)) {
-        case PNT.FIXED_LINE:
-          output.append('FIXED_LINE');
-          break;
-        case PNT.MOBILE:
-          output.append('MOBILE');
-          break;
-        case PNT.FIXED_LINE_OR_MOBILE:
-          output.append('FIXED_LINE_OR_MOBILE');
-          break;
-        case PNT.TOLL_FREE:
-          output.append('TOLL_FREE');
-          break;
-        case PNT.PREMIUM_RATE:
-          output.append('PREMIUM_RATE');
-          break;
-        case PNT.SHARED_COST:
-          output.append('SHARED_COST');
-          break;
-        case PNT.VOIP:
-          output.append('VOIP');
-          break;
-        case PNT.PERSONAL_NUMBER:
-          output.append('PERSONAL_NUMBER');
-          break;
-        case PNT.PAGER:
-          output.append('PAGER');
-          break;
-        case PNT.UAN:
-          output.append('UAN');
-          break;
-        case PNT.UNKNOWN:
-          output.append('UNKNOWN');
-          break;
-      }
+      var r = 0;
+      var v = phoneUtil.isValidNumberForRegion(number, regionCode);
+      var nr = phoneUtil.getRegionCodeForNumber(number);
+      var nt = phoneUtil.getNumberType(number);
     }
-    var PNF = i18n.phonenumbers.PhoneNumberFormat;
-    output.append('\n\n****Formatting Results:**** ');
-    output.append('\nE164 format: ');
-    output.append(isNumberValid ?
-                  phoneUtil.format(number, PNF.E164) :
-                  'invalid');
-    output.append('\nOriginal format: ');
-    output.append(phoneUtil.formatInOriginalFormat(number, regionCode));
-    output.append('\nNational format: ');
-    output.append(phoneUtil.format(number, PNF.NATIONAL));
-    output.append('\nInternational format: ');
-    output.append(isNumberValid ?
-                  phoneUtil.format(number, PNF.INTERNATIONAL) :
-                  'invalid');
-    output.append('\nOut-of-country format from US: ');
-    output.append(isNumberValid ?
-                  phoneUtil.formatOutOfCountryCallingNumber(number, 'US') :
-                  'invalid');
-    output.append('\nOut-of-country format from Switzerland: ');
-    output.append(isNumberValid ?
-                  phoneUtil.formatOutOfCountryCallingNumber(number, 'CH') :
-                  'invalid');
-    if (carrierCode.length > 0) {
-      output.append('\nNational format with carrier code: ');
-      output.append(phoneUtil.formatNationalNumberWithCarrierCode(number,
-                                                                  carrierCode));
-    }
-    output.append('\n\n****AsYouTypeFormatter Results****');
-    var formatter = new i18n.phonenumbers.AsYouTypeFormatter(regionCode);
-    var phoneNumberLength = phoneNumber.length;
-    for (var i = 0; i < phoneNumberLength; ++i) {
-      var inputChar = phoneNumber.charAt(i);
-      output.append('\nChar entered: ');
-      output.append(inputChar);
-      output.append(' Output: ');
-      output.append(formatter.inputDigit(inputChar));
-    }
+    var cc = phoneUtil.getCountryCodeForRegion(regionCode);
+    var numberArray = phoneUtil.format(
+      number, i18n.phonenumbers.PhoneNumberFormat.NATIONAL
+    ).split(" ");
+    var ac = numberArray[0].replace(/[^\d.]/g, "");
+    var sc = numberArray.slice(1).join("").replace(/[^\d.]/g, "");
+    var ds = phoneUtil.format(
+      number, i18n.phonenumbers.PhoneNumberFormat.INTERNATIONAL
+    );
   } catch (e) {
-    output.append('\n' + e.toString());
+    console.dir(e);
+    return false;
   }
-  $('output').value = output.toString();
-  return false;
+  return new PhoneNumber(p,r,v,nr,nt,cc,ac,sc,ds);
 }
-
+window['PhoneNumber'] = PhoneNumber;
+PhoneNumber.prototype['IsPossible'] = PhoneNumber.prototype.IsPossible;
+PhoneNumber.prototype['Reason'] = PhoneNumber.prototype.Reason;
+PhoneNumber.prototype['IsValid'] = PhoneNumber.prototype.IsValid;
+PhoneNumber.prototype['NumberRegion'] = PhoneNumber.prototype.NumberRegion;
+PhoneNumber.prototype['NumberType'] = PhoneNumber.prototype.NumberType;
+PhoneNumber.prototype['CountryCode'] = PhoneNumber.prototype.CountryCode;
+PhoneNumber.prototype['AreaCode'] = PhoneNumber.prototype.AreaCode;
+PhoneNumber.prototype['StationCode'] = PhoneNumber.prototype.StationCode;
+PhoneNumber.prototype['DialString'] = PhoneNumber.prototype.DialString;
 goog.exportSymbol('phoneNumberParser', phoneNumberParser);
+
